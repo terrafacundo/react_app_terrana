35372340fbc57488c4c70149ddb58fb223bb5c51
disenofgti <terra.facundo.devacc@gmail.com>
2022-06-02 08:10:04 -0300

    Navbar(componente) sumado con estilos

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () =>(
+    <header>
+        <a href="index.html">
+            <img src="https://web.dm.uba.ar/images/logo_clean_wide.png"></img>
+        </a>
+        <ul>
+            <li><a id="link" href="#">Inicio</a></li>
+            <li><a id="link" href="#">Institucional</a></li>
+            <li><a id="link" href="#">Enseñanza</a></li>
+            <li><a id="link" href="#">Extensión</a></li>
+            <li><a id="link" href="#">Investigación</a></li>
+        </ul>
+    </header>
+);
+
+export default NavBar
